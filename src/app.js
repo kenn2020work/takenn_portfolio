17f80a02e3f2e66115afc89a92e648a56ffa7457
takashi388412 <take388412@gmail.com>
2020-03-10 16:30:20 +0900

    add scroll-button to top

--- src/app.js
+++ src/app.js
@@ -1,32 +1,51 @@
 $(function () {
 
+  //scroll-top
+  var topScroll = $('.scroll-top');
+  topScroll.click(function () {
+    $('body, html').animate({ scrollTop: 0 }, 500);
+    return false;
+  });
+
   //下scrollで画像を閉じる
   $(window).scroll(function () {
     var scroll = $(this).scrollTop();
+    var scrollTop = $('.scroll-top');
     // console.log("スクロール量：" + scroll);
     $(".top-image").css({
       height: 500 - scroll
     });
-
     //scrollして画像が0pxになった時にdisplay: none;にする
     if (scroll < 500) {
       $(".site-title").removeClass("none");
+      scrollTop.css("display","none"); //scrolltopを非表示
       // console.log("Scroll：500未満です")
     } else {
       $(".site-title").addClass("none");
+      scrollTop.css({
+        "display": "block",
+        "z-index": "10"
+      }); //scrolltopを非表示
       // console.log("Scroll：500以上です")
     }
     
-    //画面幅に合わせて
+    //画面幅に合わせた処理
     jQuery(document).ready(function () {
-      //560px以下
+      // scrollTop.hide();
+      //画面幅:560px以下
       if (window.matchMedia('(max-width:560px)').matches) {
         $(function () {
+          //scroll量が500以下
           if (scroll < 500) {
-            $(".header-2").removeClass("none");
+            // $(".header-2").removeClass("none");
+            $(".header-logo").removeClass("none");
+            $(".nav-list").removeClass("none");
             $(".sticky-header").css("display","none");
-          } else {
-            $(".header-2").addClass("none");
+          } //scroll量が500以上
+          else {
+            // $(".header-2").addClass("none");
+            $(".header-logo").addClass("none");
+            $(".nav-list").addClass("none");
             $(".sticky-header").removeClass("none");
             $(".sticky-header").css({
               'display': 'block',
@@ -35,11 +54,14 @@ $(function () {
             });
           }
         });
-      } //560px以上
+      } 
+      //画面幅:560px以上
       else {
-        $(".header-2").removeClass("none");
+        $(".header-logo").removeClass("none");
+        $(".nav-list").removeClass("none");
       }
   });
+    
 
     //ふわっと現れる1
     $(".effect").each(function () {
